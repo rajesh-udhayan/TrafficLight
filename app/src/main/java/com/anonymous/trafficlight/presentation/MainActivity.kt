7f046a31c9d9af3d3cd369237ec2d95982450562
rajesh.udhayan <70341492+rajesh-udhayan@users.noreply.github.com>
2022-09-04 16:07:39 +0530

    MainActivity created and car model view integrated to the activity with app theme.

---
+++ app/src/main/java/com/anonymous/trafficlight/presentation/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.anonymous.trafficlight.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import com.anonymous.trafficlight.presentation.car_model.CarModelView
+import com.anonymous.trafficlight.presentation.theme.AppTheme
+
+class MainActivity: ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AppTheme {
+                Surface(color = MaterialTheme.colors.background) {
+                    CarModelView()
+                }
+            }
+        }
+    }
+}
--- app/src/main/java/com/anonymous/trafficlight/presentation/theme/Colors.kt
+++ app/src/main/java/com/anonymous/trafficlight/presentation/theme/Colors.kt
@@ -3,3 +3,5 @@ package com.anonymous.trafficlight.presentation.theme
 import androidx.compose.ui.graphics.Color
 
 val Blue400 = Color(0xFF42A5F5)
+val Blue700 = Color(0xFF1976D2)
+val Red400  = Color(0xFF2083AA)
