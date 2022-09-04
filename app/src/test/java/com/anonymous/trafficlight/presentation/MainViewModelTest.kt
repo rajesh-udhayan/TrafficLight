ddeeec3bfff097069e490b1f50432edc6a5c7a19
rajesh.udhayan <70341492+rajesh-udhayan@users.noreply.github.com>
2022-09-04 19:31:36 +0530

    Refactoring for readability

--- app/src/test/java/com/anonymous/trafficlight/presentation/MainViewModelTest.kt
+++ app/src/test/java/com/anonymous/trafficlight/presentation/MainViewModelTest.kt
@@ -9,27 +9,33 @@ import com.google.common.truth.Truth.assertThat
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
+import org.junit.Before
 import org.junit.Test
 
 class MainViewModelTest {
 
+    private lateinit var viewModel: MainViewModel
+    private lateinit var validator: CarModelValidator
+
+    @Before
+    fun setUp() {
+        validator = mockk()
+        viewModel = MainViewModel(validator)
+
+    }
+
     @Test
     fun `should invoke validate method in validator when view model method called`() {
-        val carModelValidator: CarModelValidator = mockk()
-        val viewModel = MainViewModel(carModelValidator)
+        every { validator.validate("test") } returns success
 
-        every { carModelValidator.validate("test") } returns success
         val result = viewModel.validateCarModel("test")
 
-        verify { carModelValidator.validate("test") }
+        verify { validator.validate("test") }
         assertThat(result).isEqualTo(success)
     }
 
     @Test
     fun `should return gray for red light when traffic light state is not red`(){
-        val carModelValidator: CarModelValidator = mockk()
-        val viewModel = MainViewModel(carModelValidator)
-
         val result = viewModel.getRedLightColor(TrafficLightState.ORANGE)
 
         assertThat(result).isEqualTo(gray)
@@ -37,9 +43,6 @@ class MainViewModelTest {
 
     @Test
     fun `should return red for red light when traffic light state is red`(){
-        val carModelValidator: CarModelValidator = mockk()
-        val viewModel = MainViewModel(carModelValidator)
-
         val result = viewModel.getRedLightColor(TrafficLightState.RED)
 
         assertThat(result).isEqualTo(red)
