224d0891af18f9ffc6b5c20c047ee4495504c731
rajesh.udhayan <70341492+rajesh-udhayan@users.noreply.github.com>
2022-09-04 16:40:48 +0530

    Hilt test runner classed added to run instrumentation tests on composables

--- app/src/androidTest/java/com/anonymous/trafficlight/presentation/car_model/CarModelViewTest.kt
+++ app/src/androidTest/java/com/anonymous/trafficlight/presentation/car_model/CarModelViewTest.kt
@@ -1,24 +1,29 @@
 package com.anonymous.trafficlight.presentation.car_model
 
 import androidx.compose.ui.test.assertIsDisplayed
-import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
-import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import com.anonymous.trafficlight.commons.Constant
+import com.anonymous.trafficlight.presentation.MainActivity
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
 
-@RunWith(AndroidJUnit4ClassRunner::class)
+@HiltAndroidTest
 class CarModelViewTest {
 
-    @get:Rule
-    val composeTestRule = createComposeRule()
+    @get:Rule(order = 1)
+    val hiltTestRule = HiltAndroidRule(this)
+
+    @get:Rule(order = 2)
+    val composeTestRule = createAndroidComposeRule<MainActivity>()
 
     @Before
     fun setUp() {
+        hiltTestRule.inject()
         composeTestRule.setContent {
             CarModelView()
         }
