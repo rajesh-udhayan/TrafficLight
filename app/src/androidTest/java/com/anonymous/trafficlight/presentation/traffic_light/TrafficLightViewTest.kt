7dde3432aec48a27baa2c12ac0d7b4489cfbe6b4
rajesh.udhayan <70341492+rajesh-udhayan@users.noreply.github.com>
2022-09-04 18:10:20 +0530

    Traffic light view created. UI test passed for showing app bar title.

---
+++ app/src/androidTest/java/com/anonymous/trafficlight/presentation/traffic_light/TrafficLightViewTest.kt
@@ -0,0 +1,43 @@
+package com.anonymous.trafficlight.presentation.traffic_light
+
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import com.anonymous.trafficlight.R
+import com.anonymous.trafficlight.presentation.MainActivity
+import com.anonymous.trafficlight.presentation.TrafficLightView
+import com.anonymous.trafficlight.presentation.theme.AppTheme
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+@HiltAndroidTest
+class TrafficLightViewTest {
+
+    @get:Rule(order = 1)
+    val hiltTestRule = HiltAndroidRule(this)
+
+    @get:Rule(order = 2)
+    val composeTestRule = createAndroidComposeRule<MainActivity>()
+
+    @Before
+    fun setUp() {
+        hiltTestRule.inject()
+        composeTestRule.setContent {
+            AppTheme {
+                TrafficLightView()
+            }
+        }
+    }
+
+    @Test
+    fun shouldDisplayUIElements() {
+        with(composeTestRule) {
+            val title = onNodeWithText(activity.getString(R.string.app_name))
+
+            title.assertIsDisplayed()
+        }
+    }
+}
---
+++ app/src/main/java/com/anonymous/trafficlight/presentation/TrafficLightView.kt
@@ -0,0 +1,25 @@
+package com.anonymous.trafficlight.presentation
+
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import com.anonymous.trafficlight.R
+
+@Composable
+fun TrafficLightView() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = stringResource(id = R.string.app_name)
+                    )
+                }
+            )
+        }
+    ) {
+
+    }
+}
