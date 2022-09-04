7527e70866b60964db3b7d5c2777029bb8cb65cc
rajesh.udhayan <70341492+rajesh-udhayan@users.noreply.github.com>
2022-09-04 12:56:34 +0530

    UI Test passed for showing car model text field.

--- app/src/androidTest/java/com/anonymous/trafficlight/presentation/car_model/CarModelViewTest.kt
+++ app/src/androidTest/java/com/anonymous/trafficlight/presentation/car_model/CarModelViewTest.kt
@@ -2,6 +2,7 @@ package com.anonymous.trafficlight.presentation.car_model
 
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import org.junit.Before
@@ -28,6 +29,15 @@ class CarModelViewTest {
             val title = onNodeWithText("Traffic Light")
 
             title.assertIsDisplayed()
+        }
+    }
+
+    @Test
+    fun shouldDisplayCarModelTextField(){
+        with(composeTestRule){
+            val carModeltextField = onNodeWithTag("car_model_text_field")
+
+            carModeltextField.assertIsDisplayed()
         }
     }
 }
--- app/src/main/java/com/anonymous/trafficlight/presentation/car_model/CarModelView.kt
+++ app/src/main/java/com/anonymous/trafficlight/presentation/car_model/CarModelView.kt
@@ -1,12 +1,32 @@
 package com.anonymous.trafficlight.presentation.car_model
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.TextField
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.text.input.TextFieldValue
 
 @Composable
 fun CarModelView() {
+    var carModel by remember {
+        mutableStateOf(TextFieldValue(""))
+    }
+
     Scaffold {
-        Text(text = "Traffic Light")
+        Column {
+            Text(text = "Traffic Light")
+            TextField(modifier = Modifier
+                .fillMaxWidth()
+                .testTag("car_model_text_field"),
+                value = carModel,
+                onValueChange = {
+                    carModel = it
+                })
+        }
+
     }
 }
